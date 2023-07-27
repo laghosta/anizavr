90a66759e32c651a0508feb4e6918526981bef5c
Olexandr <prikololeksandr@gmail.com>
2023-07-27 15:55:30 +0300

    anizavr v1.0 (timestamps are disabled)
    added loader on top anime page

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -4,9 +4,11 @@ import { AnimePreview } from "@/utils/AnimeApi";
 import { Link } from "@/utils/Link";
 import React, { useEffect, useState } from "react";
 import {Button} from "@/components/ui/button";
+import Loader from "@/components/Loader/Loader";
 const BrowsingAnime = () => {
     const [anime, setAnime] = useState<AnimePreview[]>([]);
     const [page, setPage] = useState(1);
+    const [loading,setLoading] = useState(true)
 
     const getAnime = (page: number) => {
         fetch(`${Link}/api/getPopularAnime?limit=50&page=${page}`, {
@@ -18,6 +20,7 @@ const BrowsingAnime = () => {
         })
             .then((res) => res.json())
             .then((res) => setAnime([...anime, ...res]));
+        setLoading(false)
     };
 
     useEffect(() => {
@@ -28,17 +31,24 @@ const BrowsingAnime = () => {
         getAnime(page);
     }, [page]);
     return (
-        <div className="h-full w-full flex flex-col px-10">
-            <div className="grid pt-10 gap-y-8 animePage">
-                {anime &&
-                    anime.map((el, id) => (
-                        <Anime rated={id + 1} key={id} element={el} />
-                    ))}
+        <>
+        {
+            loading ?
+                <Loader/>
+                :
+                <div className="h-full w-full flex flex-col px-10">
+                <div className="grid pt-10 gap-y-8 animePage">
+                    {anime &&
+                        anime.map((el, id) => (
+                            <Anime rated={id + 1} key={id} element={el} />
+                        ))}
+                </div>
+                <Button onClick={() => setPage(page+1)} variant="default"  className={"mt-5"}>
+                    Еще...
+                </Button>
             </div>
-            <Button onClick={() => setPage(page+1)} variant="default"  className={"mt-5"}>
-                Еще...
-            </Button>
-        </div>
+        }
+        </>
     );
 };
 export default BrowsingAnime;
