809b40c98b65baaba009f60f792950a6ad000565
Olexandr <prikololeksandr@gmail.com>
2023-08-04 01:49:51 +0300

    changed image on loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -8,7 +8,7 @@ const Loader = () => {
     return (
         <div style={{display:`${loading ? "flex" : "none"}`}} className="wrapper">
             <div className="content">
-                    <Image draggable={false} className="paw" src='/images/paw.png' alt="loading image" width={100} height={100}/>
+                    <img draggable={false} className="paw" src='/images/paw.png' alt="loading image" width={100} height={100}/>
             </div>
         </div>
     );
